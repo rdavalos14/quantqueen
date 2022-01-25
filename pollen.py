b84276fddeb3de6f848fc1915c01c6d6011fbc7b
nafets33 <stapinski89@gmail.com>
2022-01-25 13:53:58 -0500

    Variables Intro

---
+++ pollen.py
@@ -0,0 +1,38 @@
+# variables
+
+""" Logic of Weights 
+# create 3 weights on how much to place buy
+# 3 weights on time of day (9-10:30, 10:30-Noon, Noon-2:30, 2:30-4)
+# 1yr indicates Day_Allocation from Total Funds (2 funds, Long, Day)
+# RSI (20-50 % weight for Trade_Funds)
+# Mac (50-80 % weight for Trade_Funds)
+    > if Both 5day and 1day point up vs down
+    > if they contradict lean to 1 days and lower Trade_Funds
+    > If VIX in between relative mean lower Trade_Funds
+    > If % profit equal
+    > If Momentum equal lower_short  
+"""
+
+1year_MacSE = 
+1year_MacLE = 
+5day_MacSE = 
+5day_MacLE = 
+1day_MacSE = 
+1day_MacLE = 
+
+1year_RSI = 
+1year_RSI = 
+5day_RSI = 
+5day_RSI = 
+1day_RSI = 
+1day_RSI = 
+
+Momentum =
+
+Acct_Bal =
+Day_Allocation =
+Trade_Funds =
+Short_Fund_Weight = 
+Long_Fund_Weight = 
+
+
