d24fdb7e3aa69cbcae3e18a0f1c54ddbd49d631a
sstapinski <sstapinski@roku.com>
2022-02-11 16:42:13 -0500

    working

--- Hive_Utils.py
+++ Hive_Utils.py
@@ -136,7 +136,7 @@ def submit_order(symbol, qty, side, type, limit_price, client_order_id, time_in_
         submit_order()
 
 
-def return_account_info(api):
+def account_info(api):
     info = api.get_account()
             # Account({   'account_blocked': False,
             #     'account_number': '603397580',
@@ -178,16 +178,32 @@ def return_account_info(api):
             'sma': info.sma}
 
 
-def return_barset(api, SYMBOL, timeframe, time_limit):
+def return_bars(api, symbol, timeframe, start_date, end_date):
     # SYMBOL = 'SPY'
-    # timeframe = 'minute' # day, minute
-    # time_limit = 500
-    ticker = api.get_barset('{}'.format(SYMBOL), '{}'.format(timeframe), limit=time_limit) # return as df (time, open, high, low, close)
-    df = ticker.df
-    ticker_data = df['{}'.format(SYMBOL)].reset_index()
-    macd = ticker_data.ta.macd(close='close', fast=12, slow=26, append=True)
-    print(ticker_data.iloc[499])
+    # time = 1
+    # timeframe = tradeapi.TimeFrame(1, tradeapi.TimeFrameUnit.Minute) # every second
+    
+    ticker = api.get_bars(symbol, timeframe, start_date, end_date) # '2022-02-11', '2022-02-11'
+    df = ticker.df.reset_index()
+
+    macd = df.ta.macd(close='close', fast=12, slow=26, append=True)
+    # print(df.iloc[-1])
+
     return 
+
+
+def return_latest_quote(api, symbol):
+    quote_dict = api.get_latest_quote(symbol)
+    # QuoteV2({   'ap': 448.27,
+    #     'as': 3,
+    #     'ax': 'X',
+    #     'bp': 448.25,
+    #     'bs': 4,
+    #     'bx': 'T',
+    #     'c': ['R'],
+    #     't': '2022-02-11T16:19:51.467033352Z',    
+    #     'z': 'B'})
+
 
 # # Return order Status
 # def clientId_order_status(api, client_id_order):
@@ -200,17 +216,149 @@ def return_barset(api, SYMBOL, timeframe, time_limit):
 
 
 
-def convert_nano_utc_timestamp_to_est_datetime(19_digit_trc_time):
+def convert_nano_utc_timestamp_to_est_datetime(digit_trc_time):
     time = 1644523144856422000
-    dt = datetime.datetime.utcfromtimestamp(19_digit_trc_time // 1000000000) # 9 zeros
+    dt = datetime.datetime.utcfromtimestamp(digit_trc_time // 1000000000) # 9 zeros
     dt = dt.strftime('%Y-%m-%d %H:%M:%S')
     return dt
 
 
-def read_db(db_root, symbol=False):
+def read_csv_db(db_root, symbol=False):
     # spy_stream
     # spy_barset
+    tables = ['main_orders.csv', '_stream.csv', '_bars.csv']
+    for t in tables:
+        if os.path.exists(os.path.join(db_root, t)):
+            pass
+        else:
+            with open(os.path.join(db_root, t), 'w') as f:
+                print(t, "created")
+                print(f)
+
     if symbol:
-        stream = pd.read_csv(os.path.join(db_root, symbol + '_stream'), dtype=str, encoding='utf8', engine='python')
-        barset = pd.read_csv(os.path.join(db_root, symbol + '_barset'),  dtype=str, encoding='utf8', engine='python')
-    return [stream, barset]
+        stream = pd.read_csv(os.path.join(db_root, symbol + '_stream.csv'), dtype=str, encoding='utf8', engine='python')
+        bars = pd.read_csv(os.path.join(db_root, symbol + '_bars.csv'),  dtype=str, encoding='utf8', engine='python')
+        orders = pd.read_csv(os.path.join(db_root, 'main_orders.csv'),  dtype=str, encoding='utf8', engine='python')
+    return [stream, bars, orders]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" entity_v2.py Alpaca symbol matching"""
+# trade_mapping_v2 = {
+#     "i": "id",
+#     "S": "symbol",
+#     "c": "conditions",
+#     "x": "exchange",
+#     "p": "price",
+#     "s": "size",
+#     "t": "timestamp",
+#     "z": "tape",  # stocks only
+#     "tks": "takerside"  # crypto only
+# }
+
+# quote_mapping_v2 = {
+#     "S":  "symbol",
+#     "x": "exchange",  # crypto only
+#     "ax": "ask_exchange",
+#     "ap": "ask_price",
+#     "as": "ask_size",
+#     "bx": "bid_exchange",
+#     "bp": "bid_price",
+#     "bs": "bid_size",
+#     "c":  "conditions",  # stocks only
+#     "t":  "timestamp",
+#     "z":  "tape"  # stocks only
+# }
+
+# bar_mapping_v2 = {
+#     "S":  "symbol",
+#     "x": "exchange",  # crypto only
+#     "o":  "open",
+#     "h":  "high",
+#     "l":  "low",
+#     "c":  "close",
+#     "v":  "volume",
+#     "t":  "timestamp",
+#     "n":  "trade_count",
+#     "vw": "vwap"
+# }
+
+# status_mapping_v2 = {
+#     "S":  "symbol",
+#     "sc": "status_code",
+#     "sm": "status_message",
+#     "rc": "reason_code",
+#     "rm": "reason_message",
+#     "t":  "timestamp",
+#     "z":  "tape"
+# }
+
+# luld_mapping_v2 = {
+#     "S": "symbol",
+#     "u": "limit_up_price",
+#     "d": "limit_down_price",
+#     "i": "indicator",
+#     "t": "timestamp",
+#     "z": "tape"
+# }
+
+# cancel_error_mapping_v2 = {
+#     "S": "symbol",
+#     "i": "id",
+#     "x": "exchange",
+#     "p": "price",
+#     "s": "size",
+#     "a": "cancel_error_action",
+#     "z": "tape",
+#     "t": "timestamp",
+# }
+
+# correction_mapping_v2 = {
+#     "S": "symbol",
+#     "x": "exchange",
+#     "oi": "original_id",
+#     "op": "original_price",
+#     "os": "original_size",
+#     "oc": "original_conditions",
+#     "ci": "corrected_id",
+#     "cp": "corrected_price",
+#     "cs": "corrected_size",
+#     "cc": "corrected_conditions",
+#     "z": "tape",
+#     "t": "timestamp",
+# }
