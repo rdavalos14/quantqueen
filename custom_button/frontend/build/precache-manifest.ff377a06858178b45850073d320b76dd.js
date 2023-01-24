32b2db36f0ba02b284bbe4a9bddffddcfec2c576
akash3106 <akash.sharma3106@gmail.com>
2023-01-25 00:05:35 +0530

    CustomButton commit

---
+++ custom_button/frontend/build/precache-manifest.ff377a06858178b45850073d320b76dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9984cb1153f0215fe51a1f254bae8d00",
+    "url": "./index.html"
+  },
+  {
+    "revision": "178d6879b4dad6ee44cc",
+    "url": "./static/js/2.e2287c3f.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.e2287c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375281f0a4d4c46f8aae",
+    "url": "./static/js/main.92a3461d.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
