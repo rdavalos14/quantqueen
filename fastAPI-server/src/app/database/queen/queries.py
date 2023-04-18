b4445413694c2fd4e32f73a669bf44c1d923d7b7
nafets33 <stapinski89gmail.com>
2023-04-18 17:07:52 -0400

    fastapi sell get set

--- fastAPI-server/src/app/database/queen/queries.py
+++ fastAPI-server/src/app/database/queen/queries.py
@@ -2,8 +2,60 @@
 
 import pandas as pd
 import random
-from helpers.utils import ReadPickleData, find_folder, db_folder_path
+from helpers.utils import ReadPickleData, find_folder, db_folder_path, PickleData
+from datetime import datetime
+import pytz
+est = pytz.timezone("US/Eastern")
 
+def return_timestamp_string(format="%Y-%m-%d %H-%M-%S %p {}".format(est), tz=est):
+    return datetime.now(tz).strftime(format)
+
+def create_AppRequest_package(request_name, archive_bucket=None, client_order_id=None):
+    now = datetime.now(est)
+    return {
+    'client_order_id': client_order_id,
+    'app_requests_id': f'{request_name}{"_app-request_id_"}{return_timestamp_string()}{now.microsecond}', 
+    'request_name': request_name,
+    'archive_bucket': archive_bucket,
+    'request_timestamp': now,
+    }
+
+
+def app_Sellorder_request(username, prod, client_order_id, number_shares):
+  QUEEN_KING = load_queen_App_pkl(username, prod)
+  QUEEN = load_queen_pkl(username, prod)
+  queen_order = QUEEN['queen_orders']
+  df = queen_order.loc[client_order_id]
+  # check against available_shares and validate amount
+  # if number_shares > 0: # validate
+  
+  status = None
+  if len(df) > 0:
+      current_requests = [i for i in QUEEN_KING['sell_orders'] if client_order_id in i.keys()]
+      if len(current_requests) > 0:
+          status = "You Already Requested Queen To Sell order, Refresh Orders to View latest Status"
+      else:
+          sell_package = create_AppRequest_package(request_name='sell_orders', client_order_id=client_order_id)
+          sell_package['sell_qty'] = float(queen_order.get('qty_available'))
+          sell_package['side'] = 'sell'
+          sell_package['type'] = 'market'
+          QUEEN_KING['sell_orders'].append(sell_package)
+          PickleData(QUEEN_KING.get('source'), QUEEN_KING)
+          status = f'{client_order_id} : Selling Order Sent to Queen Please wait for Queen to process, Refresh Table'
+  else:
+      status = None
+
+  return {'status': status}
+
+def load_queen_App_pkl(username, prod):
+  print(db_folder_path)
+  if prod == False:
+    queen_pkl_path = db_folder_path+"/"+find_folder(username)+'/queen_App_sandbox.pkl'
+  else:
+    queen_pkl_path = db_folder_path+"/"+find_folder(username)+'/queen_App.pkl'
+  print(queen_pkl_path)
+  queen_pkl = ReadPickleData(queen_pkl_path)
+  return queen_pkl
 
 def load_queen_pkl(username, prod):
   print(db_folder_path)
