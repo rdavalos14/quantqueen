4f5ac13df871e134ba8bf080b281124e01afe663
nafets33 <stapinski89@gmail.com>
2025-01-16 22:21:46 -0500

    grid release

--- custom_grid/__init__.py
+++ custom_grid/__init__.py
@@ -4,7 +4,7 @@ from decouple import config
 from custom_grid.grid_options_builder import GridOptionsBuilder
 from custom_grid.JsCode import JsCode, walk_gridOptions
 
-_RELEASE = False
+_RELEASE = True
 #_RELEASE = False  #When using this, you need to start the server for the frontend using npm start on a terminal session.
 
 if not _RELEASE:
